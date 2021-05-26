2ef72999e59af58b9c3091210c9068fb2c84bef1
Andrey Vazhenov <andrejvazenov@gmail.com>
2021-05-26 12:30:36 +0700

    Initial Commit

---
+++ MyPlaceOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaceOne
+//
+//  Created by Андрей Важенов on 26.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
